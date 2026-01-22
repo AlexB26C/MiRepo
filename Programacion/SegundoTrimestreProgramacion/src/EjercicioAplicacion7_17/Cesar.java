4d34900ad5aa7cc6384d1e5ce57724e47db2e19a
AlexB26C <alex.barriga@educa.madrid.org>
2026-01-22 09:34:37 +0100

    22-01-2026 09:34:37

---
+++ Programacion/SegundoTrimestreProgramacion/src/EjercicioAplicacion7_17/Cesar.java
@@ -0,0 +1,21 @@
+package EjercicioAplicacion7_17;
+public class Cesar {
+    final String abecedario[] = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"};
+    
+    static String cifrar(String palabraACifrar, int numeroCodificar){
+        char arrayPalabraCifrada[] = new char[palabraACifrar.length()];
+        String palabraCifrada;
+        
+        palabraACifrar = palabraACifrar.toLowerCase();
+        
+        char ArrayACifrar[] = palabraACifrar.toCharArray();
+        
+        for (int i = 0; i < ArrayACifrar.length; i++){
+            if(ArrayACifrar[i + numeroCodificar] > ArrayACifrar.length){
+            
+            }
+            arrayPalabraCifrada[i] = ArrayACifrar[i + numeroCodificar];
+            palabraCifrada ;
+        }
+    }
+}
